6e75a2a6ca397f5f1bdb06c72e63167de9ac4e69
Ezequiel <ezequielafeita@gmail.com>
2024-05-10 11:06:31 -0300

    Se arreglaron ambos scripts y se terminó el trabajo

--- Trabajo_Practico_2/assets/js/tiendaDescuento.js
+++ Trabajo_Practico_2/assets/js/tiendaDescuento.js
@@ -2,15 +2,16 @@ let cantidad = parseInt(prompt("Ingrese la cantidad de productos a llevar"));
 let precio = parseInt(prompt("Ingrese el precio individual del producto"))
 let precioCantidad = precio * cantidad;
 let precioTotal;
-if ((cantidad < 1) && (precio < 1)){ "Ingrese un valor mayor a 1"}
+if (isNaN(cantidad) || isNaN(precio)){
+    console.log("Ingrese un valor válido")
+} else{if ((cantidad < 1) && (precio < 1)){ "Ingrese un valor mayor a 1"}
 else{
-if (precioCantidad >= 20000){
-    precioTotal = precioCantidad - (precioCantidad / 4);
-    console.log("Se aplica un descuento del 25% (",(precioCantidad/4),")")
-    console.log("Son ",cantidad, "unidades por $", precio," menos el descuento del 25% (", (precioCantidad/4), ")El precio total es: $", precioTotal)
+if (cantidad >= 10 && precioCantidad >= 20000){
+    precioTotal = precioCantidad - (precioCantidad * 0.15);
+    console.log("Se aplica un descuento del 15% (",(precioCantidad*0.15),")");
+    console.log("Son ",cantidad, " por $", precio,"(", precioCantidad,") menos el descuento del 15% (", (precioCantidad*0.15),"el precio total es: $", precioTotal)
 }
 else {
     console.log("No se aplica el descuento");
     console.log("Son ",cantidad, " por $", precio,", el precio total es: $", precioCantidad)
-
-}}
+}}}
